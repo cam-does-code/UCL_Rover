be137bf3c2c1c1d4570c6538bdc3e62ed7ab8c73
BennyN86 <benzdk@gmail.com>
2023-03-02 09:45:59 +0100

    update

--- rover_src/boot.py
+++ rover_src/boot.py
@@ -3,6 +3,7 @@ import network
 import espnow
 import machine
 import time
+import hcsr04
 
 p4 = machine.Pin(4)
 #servo1 = machine.PWM(p4,freq=50)
@@ -11,12 +12,12 @@ p4 = machine.Pin(4)
 # A WLAN interface must be active to send()/recv()
 sta = network.WLAN(network.STA_IF)
 sta.active(True)
-sta.disconnect()   # Because ESP8266 auto-connects to last Access Point
+#sta.disconnect()   # Because ESP8266 auto-connects to last Access Point
 
 e = espnow.ESPNow()
 e.active(True)
-peer = b'@\x91Q\x9b\x18\x98'   # MAC address of peer's wifi interface
-#peer = b'$\xd7\xeb\x0f\xc9d'   # MAC address of peer's wifi interface
+#peer = b'@\x91Q\x9b\x18\x98'   # MAC address of controller
+peer = b'$\xd7\xeb\x0f\xc9d'   # MAC address of rover
 e.add_peer(peer)
 
 servo1 = machine.PWM(machine.Pin(23), freq=50)
@@ -24,77 +25,8 @@ servo2 = machine.PWM(machine.Pin(22), freq=50)
 servo3 = machine.PWM(machine.Pin(21), freq=50)
 servo4 = machine.PWM(machine.Pin(19), freq=50)
 
-class HCSR04:
-    """
-    Driver to use the untrasonic sensor HC-SR04.
-    The sensor range is between 2cm and 4m.
-    The timeouts received listening to echo pin are converted to OSError('Out of range')
-    """
-    # echo_timeout_us is based in chip range limit (400cm)
-    def __init__(self, trigger_pin, echo_pin, echo_timeout_us=500*2*30):
-        """
-        trigger_pin: Output pin to send pulses
-        echo_pin: Readonly pin to measure the distance. The pin should be protected with 1k resistor
-        echo_timeout_us: Timeout in microseconds to listen to echo pin.
-        By default is based in sensor limit range (4m)
-        """
-        self.echo_timeout_us = echo_timeout_us
-        # Init trigger pin (out)
-        self.trigger = machine.Pin(trigger_pin, mode=machine.Pin.OUT, pull=None)
-        self.trigger.value(0)
-
-        # Init echo pin (in)
-        self.echo = machine.Pin(echo_pin, mode=machine.Pin.IN, pull=None)
-
-    def _send_pulse_and_wait(self):
-        """
-        Send the pulse to trigger and listen on echo pin.
-        We use the method `machine.time_pulse_us()` to get the microseconds until the echo is received.
-        """
-        self.trigger.value(0) # Stabilize the sensor
-        time.sleep_us(5)
-        self.trigger.value(1)
-        # Send a 10us pulse.
-        time.sleep_us(10)
-        self.trigger.value(0)
-        try:
-            pulse_time = machine.time_pulse_us(self.echo, 1, self.echo_timeout_us)
-            return pulse_time
-        except OSError as ex:
-            if ex.args[0] == 110: # 110 = ETIMEDOUT
-                raise OSError('Out of range')
-            raise ex
-
-    def distance_mm(self):
-        """
-        Get the distance in milimeters without floating point operations.
-        """
-        pulse_time = self._send_pulse_and_wait()
-
-        # To calculate the distance we get the pulse_time and divide it by 2
-        # (the pulse walk the distance twice) and by 29.1 becasue
-        # the sound speed on air (343.2 m/s), that It's equivalent to
-        # 0.34320 mm/us that is 1mm each 2.91us
-        # pulse_time // 2 // 2.91 -> pulse_time // 5.82 -> pulse_time * 100 // 582
-        mm = pulse_time * 100 // 582
-        return mm
-
-    def distance_cm(self):
-        """
-        Get the distance in centimeters with floating point operations.
-        It returns a float
-        """
-        pulse_time = self._send_pulse_and_wait()
-
-        # To calculate the distance we get the pulse_time and divide it by 2
-        # (the pulse walk the distance twice) and by 29.1 becasue
-        # the sound speed on air (343.2 m/s), that It's equivalent to
-        # 0.034320 cm/us that is 1cm each 29.1us
-        cms = (pulse_time / 2) / 29.1
-        return cms
-
-sensor1 = HCSR04(triggerpin1, echopin1)
-sensor2 = HCSR04(triggerpin2, echopin2)
+sensor1 = hcsr04(trigger_pin=33, echo_pin=32, echo_timeout_us=10000)
+sensor2 = hcsr04(trigger_pin=17, echo_pin=16, echo_timeout_us=10000)
 
 while True:
     host, msg = e.recv()
